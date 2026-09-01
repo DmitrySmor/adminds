efcccb8cf3a60bf81ca9bff229ebbbb567c434be
dmitrysmor <dmitrysmor@yandex.ru>
2026-09-01 12:20:02 +0300

    Шаг 1: создаём src/lib/colors.sh

---
+++ scripts/src/lib/colors.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# Цвета терминала
+readonly COLOR_RESET='\033[0m'
+
+readonly COLOR_RED='\033[31m'
+readonly COLOR_GREEN='\033[32m'
+readonly COLOR_YELLOW='\033[33m'
+readonly COLOR_BLUE='\033[34m'
+readonly COLOR_CYAN='\033[36m'
+readonly COLOR_WHITE='\033[37m'
+
+readonly COLOR_BOLD='\033[1m'
+
+readonly COLOR_BOLD_RED='\033[1;31m'
+readonly COLOR_BOLD_GREEN='\033[1;32m'
+readonly COLOR_BOLD_YELLOW='\033[1;33m'
+readonly COLOR_BOLD_BLUE='\033[1;34m'
+readonly COLOR_BOLD_CYAN='\033[1;36m'
+readonly COLOR_BOLD_WHITE='\033[1;37m'
