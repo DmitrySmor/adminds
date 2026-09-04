63308dd2f5dea5be74ae5f40971c47212f68b579
dmitrysmor <dmitrysmor@yandex.ru>
2026-09-04 10:31:57 +0300

    feat(workflow): добавлен базовый набор системных пакетов

--- build/nala_debian_13.sh
+++ build/nala_debian_13.sh
@@ -170,4 +170,4 @@ nala_install_packages() {
 check_root
 check_os
 update_system
-nala_install_packages curl wget git
+nala_install_packages sudo tree unzip tar gzip vim git htop curl wget apt-transport-https ca-certificates
--- scripts/src/workflows/nala_debian_13.sh
+++ scripts/src/workflows/nala_debian_13.sh
@@ -5,4 +5,4 @@ set -euo pipefail
 check_root
 check_os
 update_system
-nala_install_packages curl wget git
+nala_install_packages sudo tree unzip tar gzip vim git htop curl wget apt-transport-https ca-certificates
